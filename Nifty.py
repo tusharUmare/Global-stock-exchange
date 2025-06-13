a019e4b026567353d855cf557e6b803892e0121a
tusharUmare <tusharumare68@gmail.com>
2025-06-13 17:51:15 +0530

    Update Nifty.py

--- Nifty.py
+++ Nifty.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from sklearn.linear_model import LinearRegression
